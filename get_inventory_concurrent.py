eb657243dcbc4fc5c9e89d210f63632c6e390b3a
etoper <73050649+eflytop@users.noreply.github.com>
2021-06-28 17:19:47 +0800

    Add files via upload

---
+++ get_inventory_concurrent.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3 
+# -*- coding=utf-8 -*-
+# by EFLYTOP
+# www.eflytop.com
+
+from netmiko import ConnectHandler,NetmikoTimeoutException,NetmikoAuthenticationException 
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side
+from netmiko import ConnectHandler
+from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor
+import getpass
+import sys
+import time
+
+username = input("请输入设备用户名：")
+password = getpass.getpass(prompt='请输入密码:')
+#enpass = getpass.getpass(prompt='请输入enable密码:')
+
+failed_login_ip = []
+hostname_list = []
+sn_list = []
+uptime_list = []
+model_list = []
+os_version_list = []
+image_list = []
+
+devices = open('device.txt','r')
+ip_list = [x.strip() for x in devices.readlines()]
+devices.close()
+
+wb = Workbook()
+ws = wb.active
+ws.title = 'Inventory'
+ws['A1'] = 'Hostname'
+ws['B1'] = 'IP Address'
+ws['C1'] = 'Serial Number'
+ws['D1'] = 'Uptime'
+ws['E1'] = 'Model'
+ws['F1'] = 'OS Version'
+ws['G1'] = 'Image'
+yellowFill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
+thin_border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
+ws['A1'].fill=yellowFill
+ws['B1'].fill=yellowFill
+ws['C1'].fill=yellowFill
+ws['D1'].fill=yellowFill
+ws['E1'].fill=yellowFill
+ws['F1'].fill=yellowFill
+ws['G1'].fill=yellowFill
+
+#print输出到txt文件
+class Logger(object):
+	def __init__(self, filename="Default.log"):
+		self.terminal = sys.stdout
+		self.log = open(filename, "a")
+	def write(self, message):
+		self.terminal.write(message)
+		self.log.write(message)
+	def flush(self):
+		pass
+sys.stdout = Logger('log.txt')
+
+start_time = time.time()
+
+def get_ip_address():
+    with open('device.txt') as f:
+        addresses = f.read().splitlines()
+    return addresses
+
+def retrieve_data(ipaddr):
+    connection_info = {
+	    	'device_type': 'cisco_ios',
+	    	'ip': ipaddr,
+	    	'username': username,
+	   		'password': password,
+	    	#'port': 22,
+	   	 	#'secret': enpass,
+    }
+    try:
+    	conn = ConnectHandler(**connection_info)
+    	print('INFO: ',time.strftime('%x %X'),'已经登录设备：', ipaddr)
+    	hostname = conn.find_prompt().replace('#','')
+    	hostname_list.append(hostname)
+    	output = conn.send_command('show version', use_textfsm=True)
+    	sn = output[0]['serial'][0]
+    	sn_list.append(sn)
+    	uptime = output[0]['uptime']
+    	uptime_list.append(uptime)
+    	model = output[0]['hardware'][0]
+    	model_list.append(model)
+    	os_version = output[0]['version']
+    	os_version_list.append(os_version)
+    	image = output[0]['running_image']
+    	image_list.append(image)
+
+    except NetmikoAuthenticationException : #认证失败报错记录
+    	print('INFO: ',time.strftime('%x %X'),ipaddr,'[Error 1] Authentication failed.')
+    	failed_login_ip.append(ipaddr)
+
+    except NetmikoTimeoutException : #登录超时报错记录
+    	print('INFO: ',time.strftime('%x %X'),ipaddr,'[Error 2] Connection timed out.')
+    	failed_login_ip.append(ipaddr)
+
+    except : #未知报错记录
+    	print('INFO: ',time.strftime('%x %X'),ipaddr,'[Error 3] Unknown error.')
+    	failed_login_ip.append(ipaddr)
+
+with ThreadPoolExecutor(max_workers=100) as exe:
+    ip_addresses = get_ip_address()
+    results = exe.map(retrieve_data, ip_addresses)
+
+with open('device.txt') as f:
+    f.seek(0)
+    ip_list = f.readlines()
+    number_of_sw = len(ip_list) + 2
+    for hostname, ip, sn, uptime, model, os_version, image, row in zip(hostname_list, ip_list, sn_list, uptime_list, model_list, os_version_list, image_list, range(2, number_of_sw)):
+        ws.cell(row=row, column=1, value=hostname)
+        ws.cell(row=row, column=2, value=ip)
+        ws.cell(row=row, column=3, value=sn)
+        ws.cell(row=row, column=4, value=uptime)
+        ws.cell(row=row, column=5, value=model)
+        ws.cell(row=row, column=6, value=os_version)
+        ws.cell(row=row, column=7, value=image)
+
+dims = {}
+for row in ws.rows:
+    for cell in row:
+        cell.border=thin_border
+        if cell.value:
+            dims[cell.column_letter] = max((dims.get(cell.column, 0), len(str(cell.value))))
+
+for col, value in dims.items():
+    ws.column_dimensions[col].width = value + 5
+
+wb.save('inventory.xlsx')
+
+print('下列主机登录失败，请手动检查：')
+for i in failed_login_ip:
+	print(i)
+
+print('本次执行时长：%.2f'%(time.time()-start_time) + '秒')
+print("命令执行完毕，结果请查看log.txt文件")
